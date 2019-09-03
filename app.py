4dff67e45a4ffe9edd16287072e31be2b9408ea2
unknown <jonsibarr@gmail.com>
2019-09-03 21:16:50 +0700

    ''

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def handle_message(event):
     """ Here's all the messages will be handled and processed by the program """
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=print(attendance.attendance))
+        TextSendMessage(text=event.message.text)
 
 
 if __name__ == "__main__":
