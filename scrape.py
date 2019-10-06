e1a4ca29e57ec0935c436e44a5146f97a60e67ef
unknown <jonsibarr@gmail.com>
2019-10-07 03:41:56 +0700

    ''

--- scrape.py
+++ scrape.py
@@ -219,7 +219,7 @@ def bolos(login_data):
                     det=det.get_text(strip=True)
                     det=datetime.strptime(det, '%Y-%m-%d').date()
                     dates.append(det)
-        dates=dates.sort()
+        dates=sorted(dates)
         sorteddates=[datetime.strptime(ts, '%Y-%m-%d') for ts in dates]
         if abs(dates[1] - dates[0]) < timedelta(days=7):
             cpw=2
