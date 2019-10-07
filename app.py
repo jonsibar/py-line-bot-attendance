23d678970ff93db345c034cab91fc50456711f51
unknown <jonsibarr@gmail.com>
2019-10-07 18:49:10 +0700

    ''

--- app.py
+++ app.py
@@ -191,6 +191,10 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text='perdi')
             )
+    elif 'perdi' in text.split():
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='apa2 perdi mulu'))
 
 
 
